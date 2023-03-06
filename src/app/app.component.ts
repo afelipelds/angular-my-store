be418944f221f223ef6c82f801625b0919606892
afelipelds <afelipelds.ez@gmail.com>
2023-03-05 22:29:55 -0500

    Creación de componente product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -37,32 +37,38 @@ export class AppComponent {
 
   products: Product[] = [
     {
+      id: '1',
       name: 'El mejor juguete',
       price: 565,
       image: './assets/images/toy.jpg',
       category: 'all',
     },
     {
+      id: '2',
       name: 'Bicicleta casi nueva',
       price: 356,
       image: './assets/images/bike.jpg',
     },
     {
+      id: '3',
       name: 'Colleción de albumnes',
       price: 34,
       image: './assets/images/album.jpg',
     },
     {
+      id: '4',
       name: 'Mis libros',
       price: 23,
       image: './assets/images/books.jpg',
     },
     {
+      id: '5',
       name: 'Casa para perro',
       price: 34,
       image: './assets/images/house.jpg',
     },
     {
+      id: '6',
       name: 'Gafas',
       price: 3434,
       image: './assets/images/glasses.jpg',
