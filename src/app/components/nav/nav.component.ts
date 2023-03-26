719e55cfd45f4c4d3a5273e490cb0b6aed7d744f
afelipelds <afelipelds.ez@gmail.com>
2023-03-26 00:15:11 -0500

    Implementando Side Menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
+  activeMenu = false;
 
+  constructor() {}
+
+  ngOnIniti(): void {}
+
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 }
